e94df84dbac83f6cf890d7ab09a651016d082ac0
Yash <yashkothari2020@gmail.com>
2019-11-03 01:00:09 +0530

    Adding Eureka Server

---
+++ discovery-server/src/test/java/com/microservices/javabrains/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.javabrains;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
